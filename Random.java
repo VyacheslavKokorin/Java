3e81725983705a58e5a1aa76e64147144ea66c18
Vyacheslav Kokorin <kokorin17@gmail.com>
2022-01-25 22:40:59 +0300

    Add info

--- Random.java
+++ Random.java
@@ -1,6 +1,6 @@
 public class Random {
 	public static void main(String[] args) {
 		int randomNumber = (int)(Math.random() * 10);
-		System.out.println("Случайное число: " + randomNumber);
+		System.out.println("Случайное число от 0 до 9: " + randomNumber);
 	}
 }
